e2e6fa23df63f33438110e9bcb7a58a6656980cd
Mansi Bhayade <mansibhayade0804@gmail.com>
2024-01-10 14:14:00 +0530

    .

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # EventPro
-Our try on the event app. Final year Major Project. 
-Yayayayayayayaya
--- app/src/main/java/com/example/eventpro/Calendar.java
+++ app/src/main/java/com/example/eventpro/Calendar.java
@@ -8,6 +8,6 @@ public class Calendar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calender);
+
     }
 }
